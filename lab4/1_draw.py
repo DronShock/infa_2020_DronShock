8e210d0a2f45fa2ccb4cbe5bc07393d4eec2b3eb
Strelnikov_Andrew <strelnikov.av@phystech.edu>
2020-09-22 16:47:52 +0300

    Завершил выполнение задания №1

--- lab4/1_draw.py
+++ lab4/1_draw.py
@@ -6,18 +6,18 @@ pygame.init()
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
-d.rect(screen, (255, 0, 255), (100, 100, 200, 200))
-d.rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-d.polygon(screen, (255, 255, 0), [(100, 100), (200, 50),
-                                  (300, 100), (100, 100)])
-d.polygon(screen, (0, 0, 255), [(100, 100), (200, 50),
-                                (300, 100), (100, 100)], 5)
+d.rect(screen, (255, 255, 255), (0, 0, 400, 400))
 d.circle(screen, (255, 255, 0), (200, 175), 100)
+d.circle(screen, (0, 0, 0), (200, 175), 101, 1)
+d.line(screen, (0, 0, 0), (95, 80), (180, 140), 10)
+d.line(screen, (0, 0, 0), (220, 140), (300, 105), 10)
 d.circle(screen, (255, 0, 0), (150, 150), 20)
-d.circle(screen, (255, 0, 0), (250, 150), 20)
+d.circle(screen, (0, 0, 0), (150, 150), 21,1)
+d.circle(screen, (255, 0, 0), (250, 150), 15)
+d.circle(screen, (0, 0, 0), (250, 150), 16,1)
 d.circle(screen, (0, 0, 0), (150, 150), 10)
-d.circle(screen, (0, 0, 0), (250, 150), 10)
-
+d.circle(screen, (0, 0, 0), (250, 150), 8)
+d.rect(screen, (0, 0, 0), (145, 220, 110, 25))
 
 pygame.display.update()
 clock = pygame.time.Clock()
